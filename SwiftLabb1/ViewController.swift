62a9f292dee5fb21bd2f9f70c57ad5805f20d922
Jakob Haglöf <j.haglof56@gmail.com>
2017-02-19 12:13:04 +0100

    Initial Commit

---
+++ SwiftLabb1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLabb1
+//
+//  Created by Jakob Haglöf on 2017-02-19.
+//  Copyright © 2017 Jakob Haglöf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
