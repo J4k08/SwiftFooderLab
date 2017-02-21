ac9678b76912dc8b8fee81e21820f10d77d7448e
Jakob Haglöf <j.haglof56@gmail.com>
2017-02-21 11:00:14 +0100

    Has started the project, added some classes and trying still to sort out url to Matapi.se

---
+++ SwiftLabb1/File.swift
@@ -0,0 +1,60 @@
+//
+//  File.swift
+//  SwiftLabb1
+//
+//  Created by Jakob Haglöf on 2017-02-20.
+//  Copyright © 2017 Jakob Haglöf. All rights reserved.
+//
+
+import Foundation
+
+
+func searchQuery(searchField : String)-> [Any] {
+    
+    var stuff : [Any] = []
+    
+    let urlString = "http://matapi.se/foodstuff?query=\(searchField)&format=json&pretty=1"
+    if let safeUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+        let url = URL(string: safeUrlString) {
+        
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request)
+        { (data: Data?, respone: URLResponse?, error: Error?) in
+            
+            if let actualData = data {
+                let jsonOptions = JSONSerialization.ReadingOptions()
+                do {
+                    
+                    if let foods = try JSONSerialization.jsonObject(with: actualData, options: jsonOptions) as? [[String: Any]]  {
+                        
+                        stuff.append(foods)
+                            
+                            //if let text = first["Name"] as? String {
+                                
+                                DispatchQueue.main.async {
+                                    
+                                }
+                            //}
+
+                        
+                    }else {
+                        NSLog("Failed to cast from json")
+                    }
+                }
+                catch let parseError {
+                    NSLog("Failed to parse json: \(parseError)")
+                }
+            }else {
+                NSLog("No data recieved :´(")
+            }
+            
+        }
+        task.resume()
+        
+    } else {
+        NSLog ("Failed to create url :(")
+    }
+    return stuff
+    
+}
--- SwiftLabb1/ViewController.swift
+++ SwiftLabb1/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var stuff: [Any] = []
+    var search: String = ""
+    @IBOutlet weak var searchField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func search(_ sender: Any) {
+        performSegue(withIdentifier: "next", sender: self)
+        search = searchField.text!
+        stuff = searchQuery(searchField: search)
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
